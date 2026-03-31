8cc63526d9e13da38346260e22e641ec650d39aa
xeenxodiac <zodiaczeen@gmail.com>
2026-03-31 20:01:03 +0530

    Create project_script4.sh

---
+++ project_script4.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Script 4: Log File Analyzer
+# Author: AYUSH RANJAN
+
+LOGFILE=$1
+KEYWORD=${2:-"error"}  # Default = error
+COUNT=0
+
+# Check if file exists
+if [ ! -f "$LOGFILE" ]; then
+    echo "Error: File $LOGFILE not found."
+    exit 1
+fi
+
+# Read file line by line
+while IFS= read -r LINE; do
+    if echo "$LINE" | grep -iq "$KEYWORD"; then
+        COUNT=$((COUNT + 1))
+    fi
+done < "$LOGFILE"
+
+echo "--------------------------------"
+echo "Log Analysis Report"
+echo "--------------------------------"
+echo "Keyword '$KEYWORD' found $COUNT times in $LOGFILE"
+
+echo ""
+echo "Last 5 matching lines:"
+grep -i "$KEYWORD" "$LOGFILE" | tail -n 5
